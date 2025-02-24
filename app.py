8b15ab0be1403c6297754bf75f399c2c32b7033d
Danyel Gomes <danyeldev1@gmail.com>
2025-02-24 14:09:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -145,4 +145,5 @@ def registrar():
     return render_template('registrar.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=5000, debug=False)
